bc31ada356628f83e47dd0cbfe13c487e0d73077
pmilewski <pmilewski1991@gmail.com>
2016-12-02 19:44:22 +0100

    kursy

---
+++ angular/assignment1/app.js
@@ -0,0 +1,44 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+
+.controller('LunchCheckController', LunchCheckController);
+  LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController($scope){
+    $scope.message = "";
+    $scope.UserLunch = "";
+    $scope.error = "";
+    $scope.color = "";
+    $scope.ValidLunch = function(){
+      if ($scope.UserLunch.match(/(,\s{1,},|,,)/g)){
+        $scope.error = "Data can not be empty";
+      }else{
+        $scope.error = "";
+      }
+    };
+    $scope.checkIfTooMuch = function(){
+    $scope.splitLunch = $scope.UserLunch.split(',') ;
+      if($scope.UserLunch == ""){
+        $scope.message = "Please enter data first";
+        $scope.color = 'red';
+      }
+      else if($scope.error != ""){
+        $scope.message = "Please fix errors";
+        $scope.color = 'red';
+      }
+      else if($scope.splitLunch.length > 3){
+        $scope.message = "Too much!";
+        $scope.color = 'green';
+      }
+      else{
+        $scope.message = "Enjoy!";
+        $scope.color = 'green';
+      }
+
+    };
+
+  };
+
+})();
