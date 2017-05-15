3d323e074efa34f09f94e30317cdee3d18543828
pmilewski <pmilewski1991@gmail.com>
2017-05-15 19:31:20 +0200

    up

---
+++ novisa/data.js
@@ -0,0 +1,84 @@
+
+  data= {
+    'hauses':[{
+    nr: 'A102',
+    pietro: 2,
+    pokoje: 3,
+    powierzchnia: '73,54m2',
+    loggia_balkon_taras: 'Nie/Nie/8,15m2',
+    ogrod: 'Nie',
+    'zapytaj': '#/',
+    'pobierz': '#/'
+    },
+    {
+    nr: 'A105',
+    pietro: 5,
+    pokoje: 2,
+    powierzchnia: '48,51m2',
+    loggia_balkon_taras: 'Nie/Nie/8,15m2',
+    ogrod: 'Nie',
+    'zapytaj': '#/',
+    'pobierz': '#/'
+  },
+  {
+  nr: 'A106',
+  pietro: 1,
+  pokoje: 2,
+  powierzchnia: '54,21m2',
+  loggia_balkon_taras: 'Tak/Nie/10,12m2',
+  ogrod: 'Nie',
+  'zapytaj': '#/',
+  'pobierz': '#/'
+  },
+  {
+  nr: 'A107',
+  pietro: 9,
+  pokoje: 3,
+  powierzchnia: '87,56m2',
+  loggia_balkon_taras: 'Nie/Tak/8,15m2',
+  ogrod: 'Tak',
+  'zapytaj': '#/',
+  'pobierz': '#/'
+  },
+  {
+  nr: 'A108',
+  pietro: 3,
+  pokoje: 5,
+  powierzchnia: '73,54m2',
+  loggia_balkon_taras: 'Nie/Tak/3,33m2',
+  ogrod: 'Tak',
+  'zapytaj': '#/',
+  'pobierz': '#/'
+  },
+  {
+  nr: 'A109',
+  pietro: 5,
+  pokoje: 5,
+  powierzchnia: '120,24m2',
+  loggia_balkon_taras: 'Nie/Tak/12,04m2',
+  ogrod: 'Nie',
+  'zapytaj': '#/',
+  'pobierz': '#/'
+  },
+  {
+  nr: 'A110',
+  pietro: 4,
+  pokoje: 2,
+  powierzchnia: '73,54m2',
+  loggia_balkon_taras: 'Tak/Nie/9,20m2',
+  ogrod: 'Nie',
+  'zapytaj': '#/',
+  'pobierz': '#/'
+  },
+  {
+  nr: 'A111',
+  pietro: 2,
+  pokoje: 3,
+  powierzchnia: '72,43m2',
+  loggia_balkon_taras: 'Nie/Nie/8,15m2',
+  ogrod: 'Tak',
+  'zapytaj': '#/',
+  'pobierz': '#/'
+  },
+  ]
+  };
